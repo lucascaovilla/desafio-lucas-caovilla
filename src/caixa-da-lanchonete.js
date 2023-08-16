82b2b4778b263cb89309e8b092fda95dbf3e12cd
lucas <lucasgrisac@gmail.com>
2023-08-16 17:01:07 -0300

    Full implemented solution to the challenge

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -6,13 +6,13 @@ class CaixaDaLanchonete {
         var soma = 0;
         const pedido = itens.map(element => element.split(",")[0]);
     
-        // - Se não forem pedidos itens, apresentar mensagem "Não há itens no carrinho de compra!"
+
         if(!itens.length > 0) {
             console.log("Não há itens no carrinho de compra!");
             return "Não há itens no carrinho de compra!";
         }
         
-        // - Se a forma de pagamento não existir, apresentar mensagem "Forma de pagamento inválida!"
+
         if(Object.keys(metodos).indexOf(metodoDePagamento) < 0){
             console.log("Forma de pagamento inválida!");
             return "Forma de pagamento inválida!";
@@ -25,23 +25,16 @@ class CaixaDaLanchonete {
             var nomeItem = item[0];
             var quantidadeItem = item[1];
             
-            // - Se o código do item não existir, apresentar mensagem "Item inválido!"
             if(Object.keys(opcoes).indexOf(nomeItem) < 0){
                 console.log("Item inválido!");
                 return "Item inválido!";
             };
             
-            // - Se a quantidade de itens for zero, apresentar mensagem "Quantidade inválida!".
             if(quantidadeItem <= 0){
                 console.log("Quantidade inválida!");
                 return "Quantidade inválida!";
             }
     
-            
-            // - Caso item extra seja informado num pedido que não tenha o respectivo item principal
-            //   apresentar mensagem "Item extra não pode ser pedido sem o principal".
-            // - Combos não são considerados como item principal.
-            // - É possível pedir mais de um item extra sem precisar de mais de um principal.
             if(nomeItem == "chantily" && (pedido.indexOf("cafe") < 0) || nomeItem == "queijo" && (pedido.indexOf("sanduiche") < 0)){
                 console.log("Item extra não pode ser pedido sem o principal")
                 return "Item extra não pode ser pedido sem o principal";
